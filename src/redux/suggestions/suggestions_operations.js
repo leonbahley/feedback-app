cba2ff529724afeb55cf3e42e5877b8acd2c3c34
leonbahley <leon.bahley@icloud.com>
2023-03-05 21:58:08 +0100

    Update suggestions_operations.js

--- src/redux/suggestions/suggestions_operations.js
+++ src/redux/suggestions/suggestions_operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://feedback-app-gl6v.onrender.com";
 
 export const fetchSuggestions = createAsyncThunk(
   "suggestions/fetchAll",
