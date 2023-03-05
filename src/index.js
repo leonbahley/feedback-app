d3f8dbac13216848798c0869a09e9cda4fb49c51
leonbahley <leon.bahley@icloud.com>
2023-03-05 22:32:04 +0100

    fix-404-error

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./components/App";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename="/feedback-app">
+      <BrowserRouter basename="/feedback-app">
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
